dfb042b3eee857e529ddcd2a32371942e06d5027
Mingmanas Sivaraksa <62439174+AjMing@users.noreply.github.com>
2021-10-08 04:40:26 +0000

    modify main to take string input

--- test.cpp
+++ test.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
 int main(int argc, char **argv) {
+
+if(strcmp(argv[1],"bubble")==0){
+
+  cout<<"I'm doing a buble sort"<<endl;
+}
+else  cout<<"I'm not doing a buble sort"<<endl;
+
 
 int N=argc-1;
 
@@ -16,6 +24,7 @@ for(i=0;i<N;i++){
   cout<<arr[i] <<endl;
 
 }
+
 int min;
  min=arr[0];
  for(i=1;i<N;i++){
